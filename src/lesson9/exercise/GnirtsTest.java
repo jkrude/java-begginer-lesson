984107940ba2789d7548cc38a498fea900824602
jkrude <jakob.krude@hotmail.com>
2021-01-18 18:16:45 +0100

    Exercise for lesson9

---
+++ src/lesson9/exercise/GnirtsTest.java
@@ -0,0 +1,30 @@
+package lesson9.exercise;
+
+
+import static org.junit.Assert.assertEquals;
+
+import lesson5.solution.Gnirts;
+import org.junit.Test;
+
+
+/*
+ * TODO Remember the Gnirts class from lesson5?
+ *  Imagine your team mate wrote an implementation and you should test it before it can be used.
+ *  Try to design your tests without knowing the exact implementation.
+ *  Test all methods and constructors.
+ *  create a @Before method Test the basic
+ *  Use assertEquals or other Assertions methods to ensure the correctness.
+ *  Think about edge cases: What should happen if?
+ */
+public class GnirtsTest {
+
+  private Gnirts testObject;
+
+
+  @Test
+  public void lengthTest() {
+    // TODO use the right expected value ( depending on your setup method ).
+    assertEquals(0, testObject.length());
+  }
+
+}
