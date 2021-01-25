4dbd9ea3cb1808adc012e90a7ec8ad6837b0ff86
jkrude <jakob.krude@hotmail.com>
2021-01-25 18:14:06 +0100

    Solution for lesson9

---
+++ src/lesson9/solution/GnirtsTest.java
@@ -0,0 +1,54 @@
+package lesson9.solution;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import lesson5.solution.Gnirts;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class GnirtsTest {
+
+  private static Gnirts testObject;
+
+
+  @BeforeAll
+  public static void setUp() throws Exception {
+    // Same test condition for all tests.
+    // Reduced boilerplate code within test-methods.
+    testObject = new Gnirts("gnirtS");
+  }
+
+  @Test
+  public void lengthTest() {
+    assertEquals(6, testObject.length());
+
+  }
+
+  @Test
+  public void charAtTest() {
+    String expectedString = "String";
+    for (int i = 0; i < expectedString.length(); ++i) {
+      // Tests the base functionality too.
+      assertEquals(expectedString.charAt(i), testObject.charAt(i));
+    }
+  }
+
+
+  @Test
+  public void subSequenceTest() {
+    // 0=inclusive, 3=exclusive (see CharSequence)
+    CharSequence expected = "Str";
+    assertEquals(expected, testObject.subSequence(0, 3));
+
+    // Possible edge-case.
+    assertEquals("", testObject.subSequence(0, 0));
+  }
+
+  @Test
+  public void subSequenceThrowTest() {
+    assertThrows(Exception.class, () -> testObject.subSequence(-4, 0)); // IndexOutOfBounds
+  }
+
+}
